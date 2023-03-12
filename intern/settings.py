b116561c08ca9591eedfa0c724464144ed42c0a3
Obuyinza20 <obtexreins2000@gmail.com>
2023-03-12 22:01:27 +0300

    more changes

--- intern/settings.py
+++ intern/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-mpg&tt12rcx^ke7h1%9)%_j@xh9ld^9t6+#v#mvu%0i2jbxh(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
